21505f64e95c67ce07eff082492465b700b9b03e
rodframeh <rmendozamelgarrodrigo@gmail.com>
2025-03-25 19:08:17 -0500

    feat(SP-002): expose and link library

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+mod dynamic;
+mod model;
+
+pub use dynamic::application::dynamic_value::DynamicValue;
+pub use model::application::model_manager::ModelManager;
+
+pub use dynamic::infrastructure::dynamic_value_impl::DynamicValueImpl;
+pub use model::infrastructure::model_manager_impl::ModelManagerImpl;
